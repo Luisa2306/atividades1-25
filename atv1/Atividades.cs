d3edc0efb6bc0b7a9463c224ecb2465a95e3dc4e
Matheus Fernandes <matheusfffcorrea@gmail.com>
2025-10-14 13:57:04 -0300

    Primeiro commit - estrutura do menu

---
+++ atv1/Atividades.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv1
+{
+    public class Atividades
+    {
+        public void At1()
+        {
+
+        }
+
+        public void At2()
+        {
+
+        }
+
+        public void At3()
+        {
+
+        }
+
+        public void At4()
+        {
+
+        }
+
+        public void At5()
+        {
+
+        }
+
+        public void At6()
+        {
+
+        }
+
+        public void At7()
+        {
+
+        }
+
+        public void At8()
+        {
+
+        }
+
+        public void At9()
+        {
+
+        }
+
+        public void At10()
+        {
+
+        }
+
+        public void At11()
+        {
+
+        }
+
+        public void At12()
+        {
+
+        }
+
+        public void At13()
+        {
+
+        }
+
+        public void At14()
+        {
+
+        }
+
+        public void At15()
+        {
+
+        }
+
+        public void At16()
+        {
+
+        }
+
+        public void At17()
+        {
+
+        }
+
+        public void At18()
+        {
+
+        }
+
+        public void At19()
+        {
+
+        }
+
+        public void At20()
+        {
+
+        }
+
+        public void At21()
+        {
+
+        }
+
+        public void At22()
+        {
+
+        }
+
+        public void At23()
+        {
+
+        }
+
+        public void At24()
+        {
+
+        }
+
+        public void At25()
+        {
+
+        }
+
+    }
+}
---
+++ atv1/Program.cs
@@ -0,0 +1,96 @@
+﻿
+
+using atv1;
+
+int escolha = 0;
+void Menu()
+{
+    Console.WriteLine("================================");
+    Console.WriteLine("Escolha uma Atividade de 1 a 25");
+    Console.WriteLine("================================");
+    Console.WriteLine("\n");
+    escolha = int.Parse(Console.ReadLine());
+}
+
+Atividades att = new Atividades();
+
+switch (escolha)
+{
+    case 1:
+        Console.WriteLine("Atividade 1:\n");
+        att.At1();
+
+        break;
+    case 2:
+        Console.WriteLine("Atividade 2:\n");
+        break;
+    case 3:
+        Console.WriteLine("Atividade 3:\n");
+        break;
+    case 4:
+        Console.WriteLine("Atividade 4:\n");
+        break;
+    case 5:
+        Console.WriteLine("Atividade 5:\n");
+        break;
+    case 6:
+        Console.WriteLine("Atividade 6:\n");
+        break;
+    case 7:
+        Console.WriteLine("Atividade 7:\n");
+        break;
+    case 8:
+        Console.WriteLine("Atividade 8:\n");
+        break;
+    case 9:
+        Console.WriteLine("Atividade 9:\n");
+        break;
+    case 10:
+        Console.WriteLine("Atividade 10:\n");
+        break;
+    case 11:
+        Console.WriteLine("Atividade 11:\n");
+        break;
+    case 12:
+        Console.WriteLine("Atividade 12:\n");
+        break;
+    case 13:
+        Console.WriteLine("Atividade 13:\n");
+        break;
+    case 14:
+        Console.WriteLine("Atividade 14:\n");
+        break;
+    case 15:
+        Console.WriteLine("Atividade 15:\n");
+        break;
+    case 16:
+        Console.WriteLine("Atividade 16:\n");
+        break;
+    case 17:
+        Console.WriteLine("Atividade 17:\n");
+        break;
+    case 18:
+        Console.WriteLine("Atividade 18:\n");
+        break;
+    case 19:
+        Console.WriteLine("Atividade 19:\n");
+        break;
+    case 20:
+        Console.WriteLine("Atividade 20:\n");
+        break;
+    case 21:
+        Console.WriteLine("Atividade 21:\n");
+        break;
+    case 22:
+        Console.WriteLine("Atividade 22:\n");
+        break;
+    case 23:
+        Console.WriteLine("Atividade 23:\n");
+        break;
+    case 24:
+        Console.WriteLine("Atividade 24:\n");
+        break;
+    case 25:
+        Console.WriteLine("Atividade 25:\n");
+        break;
+}
